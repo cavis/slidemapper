6ce01e5cf84eb36afcd40e9e528a96d7f86b0115
cavis <ryancavis@gmail.com>
2012-07-03 09:35:06 -0500

    simply simplify ... move constrainer/clusterer off into separate projects

--- slidemapper.js
+++ slidemapper.js
@@ -19,281 +19,16 @@
 
 
 /* ==========================================================
- * Constrainer control - allow limiting slides to a
- * rectangular area of the map.
- * ========================================================== */
-L.Control.Constrainer = L.Control.extend({
-  options: { position: 'topright' },
-
-  statics: {
-    START: L.Browser.touch ? 'touchstart' : 'mousedown',
-    END: L.Browser.touch ? 'touchend' : 'mouseup',
-    MOVE: L.Browser.touch ? 'touchmove' : 'mousemove'
-  },
-
-  onAdd: function (map) {
-    var className = 'smapp-control-constrain',
-        container = L.DomUtil.create('div', className);
-
-    this._link   = L.DomUtil.create('a', className + '-selection', container);
-    this._dashes = L.DomUtil.create('div', className + '-selection-inner', this._link);
-    this._link.href  = '#';
-    this._link.title = 'Constrain Selection';
-
-    // toggle state when clicking on the
-    L.DomEvent
-      .addListener(this._link, 'click', L.DomEvent.stopPropagation)
-      .addListener(this._link, 'click', L.DomEvent.preventDefault)
-      .addListener(this._link, 'click', this._onClick, this);
-    return container;
-  },
-
-  // toggle click handler
-  _onClick: function(e) {
-    if (L.DomUtil.hasClass(this._link, 'down')) {
-      L.DomUtil.removeClass(this._link, 'down');
-      this._onEndDraw();
-      if (this._rectangle) {
-        this._map.removeLayer(this._rectangle);
-        this._rectangle = false;
-      }
-    }
-    else {
-      L.DomUtil.addClass(this._link, 'down');
-      this._map.dragging._draggable.disable();
-      this._setDrawingCursor();
-      L.DomEvent.addListener(this._map._mapPane, L.Control.Constrainer.START, this._onStartDraw, this);
-    }
-  },
-
-  // mouse down handler
-  _onStartDraw: function(e) {
-    this._origin = this._map.mouseEventToLatLng(e);
-    L.DomEvent
-      .addListener(document, L.Control.Constrainer.MOVE, L.DomEvent.stopPropagation)
-      .addListener(document, L.Control.Constrainer.MOVE, L.DomEvent.preventDefault)
-      .addListener(document, L.Control.Constrainer.MOVE, this._onDraw, this)
-      .addListener(document, L.Control.Constrainer.END, this._onEndDraw, this);
-  },
-
-  // mouse moving handler
-  _onDraw: function(e) {
-    var latlng = this._map.mouseEventToLatLng(e);
-    var bounds = new L.LatLngBounds(this._origin, latlng);
-    if (this._rectangle) {
-      this._rectangle.setBounds(bounds);
-    }
-    else {
-      this._rectangle = new L.Rectangle(bounds, {
-        clickable: false,
-        fillOpacity: .1,
-        opacity: .2,
-        weight: 3
-      });
-      this._map.addLayer(this._rectangle);
-    }
-  },
-
-  // mouse up handler
-  _onEndDraw: function(e) {
-    this._map.dragging._draggable.enable();
-    this._restoreCursor();
-    L.DomEvent.removeListener(this._map._mapPane, L.Control.Constrainer.START, this._onStartDraw);
-    L.DomEvent.removeListener(document, L.Control.Constrainer.MOVE, this._onDraw);
-    L.DomEvent.removeListener(document, L.Control.Constrainer.END, this._onEndDraw);
-  },
-
-  // add global drawing cursor
-  _setDrawingCursor: function() {
-    document.body.className += ' smapp-constrain-drawing';
-  },
-
-  // remove global dragging cursor
-  _restoreCursor: function() {
-    document.body.className = document.body.className.replace(/ smapp-constrain-drawing/g, '');
-  },
-
-});
-
-L.Map.addInitHook(function() {
-  if (this.options.constrainControl) {
-    this.constrainControl = new L.Control.Constrainer();
-    this.addControl(this.constrainControl);
-  }
-});
-
-
-/* ==========================================================
- * Clustering control - allow limiting slides to a rectangular area
- * of the map.
- * ========================================================== */
-L.Control.Cluster = L.Control.extend({
-  options: {
-    position: 'topright',
-    minValue: 0,
-    maxValue: 120,
-    stepSize: 10,
-    startValue: 80
-  },
-
-  statics: {
-    START: L.Browser.touch ? 'touchstart' : 'mousedown',
-    END: L.Browser.touch ? 'touchend' : 'mouseup',
-    MOVE: L.Browser.touch ? 'touchmove' : 'mousemove',
-  },
-
-  onAdd: function (map) {
-    var className = 'smapp-control-cluster',
-        container = L.DomUtil.create('div', className);
-    this._slider   = L.DomUtil.create('div', className + '-slider', container);
-    this._range    = L.DomUtil.create('div', className + '-slider-range', this._slider);
-    this._handleCt = L.DomUtil.create('div', className + '-slider-handle-ct', this._slider);
-    this._handle   = L.DomUtil.create('span', className + '-slider-handle', this._handleCt);
-    this._handle.title = 'Clustering Size';
-
-    // initial size
-    var initialStep = this._valToStep(this.options.startValue);
-    this._setStep(initialStep);
-    this._steps = false; //get css-height on subsequent loads
-
-    // listen to dragging on the handle
-    L.DomEvent
-      .addListener(this._handle, 'click', L.DomEvent.stopPropagation)
-      .addListener(this._handle, 'click', L.DomEvent.preventDefault)
-      .addListener(this._handle, L.Control.Cluster.START, L.DomEvent.preventDefault)
-      .addListener(this._handle, L.Control.Cluster.MOVE, L.DomEvent.stopPropagation)
-      .addListener(this._handle, L.Control.Cluster.MOVE, L.DomEvent.preventDefault)
-      .addListener(this._handle, L.Draggable.START, this._onStartDrag, this);
-
-    // listen to clicking on the range
-    L.DomEvent
-      .addListener(this._slider, 'click', L.DomEvent.stopPropagation)
-      .addListener(this._slider, 'click', L.DomEvent.preventDefault)
-      .addListener(this._slider, 'click', this._onClick, this);
-    return container;
-  },
-
-  // slider click handler
-  _onClick: function(e) {
-    var step = this._yCoordToStep(e.pageY);
-    this._setStep(step);
-    this._onEndDrag();
-  },
-
-  // mouse down handler
-  _onStartDrag: function(e) {
-    this._map.dragging._draggable.disable();
-    this._setMovingCursor();
-    L.DomEvent
-      .addListener(document, L.Control.Cluster.MOVE, L.DomEvent.stopPropagation)
-      .addListener(document, L.Control.Cluster.MOVE, L.DomEvent.preventDefault)
-      .addListener(document, L.Control.Cluster.MOVE, this._onDrag, this);
-    L.DomEvent.addListener(document, L.Control.Cluster.END, this._onEndDrag, this);
-  },
-
-  // mouse moving handler
-  _onDrag: function(e) {
-    var step = this._yCoordToStep(e.pageY);
-    this._setStep(step);
-  },
-
-  // mouse up handler
-  _onEndDrag: function(e) {
-    this._map.dragging._draggable.enable();
-    this._restoreCursor();
-    L.DomEvent.removeListener(document, L.Control.Cluster.MOVE, this._onDrag);
-    L.DomEvent.removeListener(document, L.Control.Cluster.END, this._onEndDrag);
-  },
-
-  // initialize slider steps
-  _initSteps: function() {
-    var height = parseInt(L.DomUtil.getStyle(this._slider, 'height').replace('px', ''));
-    var top    = L.DomUtil.getViewportOffset(this._slider).y;
-    var bot    = top + height;
-    var half   = (.5 * (this.options.stepSize / (this.options.maxValue - this.options.minValue))) * height;
-    this._steps = [];
-    for (var i=this.options.minValue; i<=this.options.maxValue; i+=this.options.stepSize) {
-      var perc = i / (this.options.maxValue - this.options.minValue);
-      this._steps.push({y: bot - (height * perc) - half, val: i, perc: perc});
-    }
-  },
-
-  // transform a point to a step
-  _yCoordToStep: function(yCoord) {
-    if (!this._steps) this._initSteps();
-    for (var i=0; i<this._steps.length; i++) {
-      if (yCoord >= this._steps[i].y) return this._steps[i];
-    }
-    return this._steps[this._steps.length - 1];
-  },
-
-  // transform a value to a step number
-  _valToStep: function(val) {
-    if (!this._steps) this._initSteps();
-    for (var i=0; i<this._steps.length; i++) {
-      if (val <= this._steps[i].val) return this._steps[i];
-    }
-    return this._steps[this._steps.length - 1];
-  },
-
-  // helper to convert to percentage string
-  _stepToPercentage: function(step) {
-    return Math.round(100*step.perc) + '%';
-  },
-
-  // set the slider to a step
-  _setStep: function(step) {
-    this._range.style.height = this._stepToPercentage(step);
-    this._handle.style.bottom = this._stepToPercentage(step);
-    this._handle.style['font-weight'] = (step.val == 0) ? 'normal' : 'bold';
-    this._handle.innerHTML = (step.val == 0) ? 'off' : step.val;
-    this._refreshClusterer(step.val);
-  },
-
-  // refresh the leafclusterer
-  _refreshClusterer: function(newGridSize) {
-    if (this._map.clusterer) {
-      if (this._map.clusterer.getGridSize_() != newGridSize) {
-        this._map.clusterer.setGridSize_(newGridSize);
-      }
-    }
-  },
-
-  // add global dragging cursor
-  _setMovingCursor: function() {
-    document.body.className += ' smapp-slider-dragging';
-  },
-
-  // remove global dragging cursor
-  _restoreCursor: function() {
-    document.body.className = document.body.className.replace(/ smapp-slider-dragging/g, '');
-  }
-
-});
-
-L.Map.addInitHook(function() {
-  if (this.options.cluster && this.options.clusterControl) {
-    this.clusterControl = new L.Control.Cluster({
-      minValue: this.options.clusterMinGridSize,
-      maxValue: this.options.clusterMaxGridSize,
-      stepSize: this.options.clusterGridStep,
-      startValue: this.options.clusterGridSize
-    });
-    this.addControl(this.clusterControl);
-  }
-});
-
-
-/* ==========================================================
  * SlideMapper jquery extension
  *
+ * Creates a slidemapper widget with an element.
  * ========================================================== */
 (function($) {
 
   // default configuration options
   var defaultOptions = {
     // base
-    mapType: 'cloudmade',
+    mapType: 'mapquest',
     apiKey:  null,
     center:  [40.423, -98.7372],
     closePopupOnClick: false,
@@ -306,18 +41,7 @@ L.Map.addInitHook(function() {
     // display options
     autoHeight: true,
     slideMargins: true,
-    animateSpeed: 200,
-    // clustering
-    cluster: false,
-    clusterMaxZoom: 9,
-    clusterGridSize: 60,
-    // clustering control
-    clusterControl: false,
-    clusterMinGridSize: 0,
-    clusterMaxGridSize: 120,
-    clusterGridStep: 10,
-    // constraining control
-    constrainControl: false
+    animateSpeed: 200
   };
 
 
@@ -413,15 +137,6 @@ L.Map.addInitHook(function() {
   }
   function _showPopup(marker, panTo) {
     var latlng = marker.getLatLng();
-
-    // get a more favorable point for clustered markers
-    if (DATA.clusterer) {
-      if (clll = DATA.clusterer.getClusterLatLng(marker)) {
-        var pt = DATA.map.latLngToLayerPoint(clll);
-        pt.y += 20;
-        latlng = DATA.map.layerPointToLatLng(pt);
-      }
-    }
     var popup = marker._popup.setLatLng(latlng);
     DATA.map.openPopup(popup);
     if (panTo) {
@@ -478,7 +193,6 @@ L.Map.addInitHook(function() {
     init: function(passedOpts) {
       if (!DATA) {
         DATA = {};
-        if (passedOpts.cluster === false) passedOpts.clusterControl = false;
         DATA.options = $.extend({}, defaultOptions, passedOpts);
 
         // create the slideshow
@@ -493,20 +207,11 @@ L.Map.addInitHook(function() {
         // initialize the map
         DATA.options.center = new L.LatLng(DATA.options.center[0], DATA.options.center[1]);
         DATA.map = new L.Map(mapEl, DATA.options);
-        _setTiles(DATA.options.mapType);        
+        _setTiles(DATA.options.mapType);
 
-        // setup data containers
-        if (DATA.options.cluster) {
-          DATA.clusterer = new LeafClusterer(DATA.map, null, {
-            gridSize: DATA.options.clusterGridSize,
-            maxZoom: DATA.options.clusterMaxZoom
-          });
-          DATA.map.clusterer = DATA.clusterer; //give map a ref
-        }
-        else {
-          DATA.markergroup = new L.LayerGroup();
-          DATA.map.addLayer(DATA.markergroup);
-        }
+        // create a layer for the markers
+        DATA.markergroup = new L.LayerGroup();
+        DATA.map.addLayer(DATA.markergroup);
 
         // setup initial items
         DATA.items = [];
@@ -531,7 +236,7 @@ L.Map.addInitHook(function() {
       marker.on('click', function(e) {
         methods.move(e.target.index, true);
       });
-      DATA.clusterer ? DATA.clusterer.addMarker(marker) : DATA.markergroup.addLayer(marker);
+      DATA.markergroup.addLayer(marker);
 
       // render to preview
       var caro = $THIS.find('.smapp-carousel');
